731ae0d4c918d921563359c551f7c3ec0be78f11
Adrian Cantrill <acantril@Hive.local>
2019-06-10 18:06:51 +1000

    api gateway

---
+++ lesson_files/03_compute/Topic4_Serverless/apigateway/calc.js
@@ -0,0 +1,41 @@
+console.log('Loading the Calc function');
+
+exports.handler = function(event, context, callback) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    if (event.a === undefined || event.b === undefined || event.op === undefined) {
+        callback("400 Invalid Input");
+    }
+    
+    var res = {};
+    res.a = Number(event.a);
+    res.b = Number(event.b);
+    res.op = event.op;
+    
+    if (isNaN(event.a) || isNaN(event.b)) {
+        callback("400 Invalid Operand");
+    }
+
+    switch(event.op)
+    {
+        case "+":
+        case "add":
+            res.c = res.a + res.b;
+            break;
+        case "-":
+        case "sub":
+            res.c = res.a - res.b;
+            break;
+        case "*":
+        case "mul":
+            res.c = res.a * res.b;
+            break;
+        case "/":
+        case "div":
+            res.c = res.b===0 ? NaN : Number(event.a) / Number(event.b);
+            break;
+        default:
+            callback("400 Invalid Operator");
+            break;
+    }
+    callback(null, res);
+};
