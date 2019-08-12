19b4f4b4c1c3d0aaf2f4858ac7dfd294d2fb83bb
Adrian Cantrill <acantril@Hive.local>
2019-08-13 08:04:48 +1000

    fixes

--- lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/bootstrap1.sh
+++ lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/bootstrap1.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 yum install -y httpd php git
-service httpd start
-cd
-git clone https://github.com/linuxacademy/aws-csa-pro-2019.git
-cp ./aws-csa-pro-2019/05_ha_scaling_ft/ELB/* /var/www/html
+
+cd /var/www/html
+wget https://github.com/linuxacademy/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/cat1.jpg
+mv cat1.jpg cat.jpg
+wget https://github.com/linuxacademy/content-aws-csa2019/raw/master/lesson_files/07_hybrid_scaling/1_LBandASG/CLBandHealth/index.php
 mv /var/www/html/htaccess /var/www/html/.htaccess
+
+
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
